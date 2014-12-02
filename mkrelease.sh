772cbc44b0be8907fccd0b34dae83cbbc335f3b2
Mart Lubbers <mart.lubbers@mpi.nl>
2014-12-02 14:00:54 +0100

    version 0.9a

--- mkrelease.sh
+++ mkrelease.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=0.9
+VERSION=0.9a
 zip -9rv praatalign_$VERSION.zip align* cleaninterval.praat generatedict.p* install_* LICENCE* par.* phonetizer.py pyDAWG.py README.html set*
 tar -cvf praatalign_$VERSION.tar align* cleaninterval.praat generatedict.p* install_* LICENCE* par.* phonetizer.py pyDAWG.py README.html set*
 gzip -vv9c praatalign_$VERSION.tar > praatalign_$VERSION.tar.gz
