aa523ec5b542c185e791839b97761b579f9ef988
Mart Lubbers <mart.lubbers@mpi.nl>
2015-03-25 09:48:28 +0100

    updated release script

--- mkrelease.sh
+++ mkrelease.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=1.3
+VERSION=1.31
 zip -9rv praatalign_$VERSION.zip *.{praat,py,md} {install,par,LICENCE}*
 tar -cvf praatalign_$VERSION.tar *.{praat,py,md} {install,par,LICENCE}*
 gzip -vv9c praatalign_$VERSION.tar > praatalign_$VERSION.tar.gz
